858d0890d6a95342513ca9958aa7bf7715e41b55
symbat27 <242262@astanait.edu.kz>
2025-10-21 23:28:37 +0500

    Update WebTechnologiesCourse.java

--- WebTechnologiesCourse.java
+++ WebTechnologiesCourse.java
@@ -1,13 +1,9 @@
 public class WebTechnologiesCourse implements Course {
     @Override
-    public String deliverContent() {
-        return "Web Technologies content: HTML, CSS, JavaScript, React, Node.js";
-    }
-
-    @Override
-    public String getDescription() {
-        return "Web Technologies";
+    public void deliverContent() {
+        System.out.println("Web Technologies course selected.");
     }
 }
+
 
 
